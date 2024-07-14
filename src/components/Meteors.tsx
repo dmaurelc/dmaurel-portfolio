2c8df90d4cdb300a84e3854f486b9464e7db7858
DanielMC <dmaurelc@gmail.com>
2024-07-14 13:19:39 -0400

    First commit

---
+++ src/components/Meteors.tsx
@@ -0,0 +1,43 @@
+// components/magicui/meteors.tsx
+"use client";
+
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+
+interface MeteorsProps {
+  number?: number;
+}
+
+export const Meteors = ({ number = 20 }: MeteorsProps) => {
+  const [meteorStyles, setMeteorStyles] = useState<Array<React.CSSProperties>>([]);
+
+  useEffect(() => {
+    console.log("Meteors component mounted");  // Añade este console.log para verificar si el componente se monta
+    const styles = [...new Array(number)].map(() => ({
+      top: -5,
+      left: Math.floor(Math.random() * window.innerWidth) + "px",
+      animationDelay: Math.random() * 1 + 0.2 + "s",
+      animationDuration: Math.floor(Math.random() * 8 + 2) + "s",
+    }));
+    console.log(styles);
+    setMeteorStyles(styles);
+  }, [number]);
+
+  return (
+    <>
+      {[...meteorStyles].map((style, idx) => (
+        <span
+          key={idx}
+          className={clsx(
+            "pointer-events-none absolute left-1/2 top-1/2 h-0.5 w-0.5 rotate-[215deg] animate-meteor rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]",
+          )}
+          style={style}
+        >
+          <div className="pointer-events-none absolute top-1/2 -z-10 h-[1px] w-[50px] -translate-y-1/2 bg-gradient-to-r from-slate-500 to-transparent" />
+        </span>
+      ))}
+    </>
+  );
+};
+
+export default Meteors;
---
+++ tailwind.config.mjs
@@ -0,0 +1,98 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      body: ["Afacad", "Arial", "sans-serif"],
+      heading: ["Afacad", "Arial", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        brand: {
+          50: "#FFF9E5",
+          100: "#FFF2CC",
+          200: "#FFE79E",
+          300: "#FFDA6B",
+          400: "#FFCD38",
+          500: "#FFC107",
+          DEFAULT: "#FFC107",
+          600: "#D19D00",
+          700: "#9E7700",
+          800: "#6B5000",
+          900: "#332600",
+          950: "#191300",
+        },
+        accent: {
+          50: "#E4E4EC",
+          100: "#C9C9D9",
+          200: "#9797B5",
+          300: "#64648C",
+          400: "#3E3E56",
+          500: "#191923",
+          DEFAULT: "#191923",
+          600: "#13131B",
+          700: "#0F0F15",
+          800: "#0B0B0F",
+          900: "#040406",
+          950: "#020203",
+        },
+      },
+      borderRadius: {
+        "4xl": "2rem",
+        "5xl": "3rem",
+        "6xl": "5rem",
+      },
+      aspectRatio: {
+        landscape: "4 / 3",
+        portrait: "3 / 4",
+        large: "5 / 8",
+        ultrawide: "18 / 5",
+        golden: "1.6180 / 1",
+        rrss: "1.91 / 1",
+      },
+      zIndex: {
+        60: "60",
+        70: "70",
+        80: "80",
+        90: "90",
+        100: "9999",
+      },
+      gridTemplateColumns: {
+        "1-2": "minmax(0, 1fr) minmax(0, 2fr)",
+        "2-1": "minmax(0, 2fr) minmax(0, 1fr)",
+        "1-3": "minmax(0, 1fr) minmax(0, 3fr)",
+        "3-1": "minmax(0, 3fr) minmax(0, 1fr)",
+        "1-4": "minmax(0, 1fr) minmax(0, 4fr)",
+        "4-1": "minmax(0, 4fr) minmax(0, 1fr)",
+        "2-3": "minmax(0, 2fr) minmax(0, 3fr)",
+        "3-2": "minmax(0, 3fr) minmax(0, 2fr)",
+        "1-2-1": "minmax(0, 1fr) minmax(0, 2fr) minmax(0, 1fr)",
+        "1-4-1": "minmax(0, 1fr) minmax(0, 4fr) minmax(0, 1fr)",
+      },
+      animation: {
+        'border': 'border 4s linear infinite',
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+        'meteor': "meteor 5s linear infinite",
+      },
+      keyframes: {
+          'border': {
+              to: { '--border-angle': '360deg' },
+          },
+          'infinite-scroll': {
+            from: { transform: 'translateX(0)' },
+            to: { transform: 'translateX(-100%)' },
+          },
+          'meteor': {
+            "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
+            "70%": { opacity: 1 },
+            "100%": {
+              transform: "rotate(215deg) translateX(-500px)",
+              opacity: 0,
+            },
+          },
+      },     
+    },
+  },
+  plugins: ["prettier-plugin-tailwindcss", require("tailwindcss-animated")],
+};
